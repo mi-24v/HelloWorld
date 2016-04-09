8fd2923b195533c3edd59c23d59bb44abfdc6e2d
mi-24v <yuusukessid@gmail.com>
2016-04-09 11:04:26 +0900

    Added Hello World

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,13 @@
+﻿#include<iostream>
+
+int main() {
+	std::cout << "Hello, World!!!!" << std::endl;
+	std::cout << "うんちっていうのは、まだ完全に消化されてないから、栄養があるんだ。" << std::endl;
+
+	while (true)
+	{
+		//Something to do in main loop
+	}
+
+	return 0;
+}
